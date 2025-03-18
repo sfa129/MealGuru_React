5d8c68e5b1f2a1ba8e03c084d18e8732c8db9d76
Syed Faiz Ahmed <sfahmed129@gmail.com>
2025-03-18 23:16:39 +0500

    setup

---
+++ mealguru_react/src/assets/images/index.js
@@ -0,0 +1,33 @@
+import boximage from './images/boximage'
+import accimg from './accimg';
+import chooseimg from './chooseimg';
+import facebook from './facebook';
+import feed1 from './feed1';
+import feed2 from './feed2';
+import feed3 from './feed3';
+import instagram from './instagram';
+import logo from './logo';
+import section1 from './section1';
+import section2 from './section2';
+import section3 from './section3';
+import section4 from './section4';
+import tiktok from './tiktok';
+import youtube from '../imagesyoutube';
+
+export {
+    boximage,
+    accimg,
+    chooseimg,
+    facebook,
+    feed1,
+    feed2,
+    feed3,
+    instagram,
+    logo,
+    section1,
+    section2,
+    section3,
+    section4,
+    tiktok,
+    youtube
+}
